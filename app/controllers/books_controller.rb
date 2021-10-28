a57af2fb0768757e9e247e4404df78b5f2bcf97c
yash001dev <y.sanghavi001@gmail.com>
2021-10-28 18:10:37 +0530

    Book controller complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,27 @@
 class BooksController < ApplicationController
+  before_action :get_author
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all
+    @books = @author.books
   end
 
   def show
   end
 
   def new
-    @book = Book.new
+    @book = @author.books.build
   end
 
   def edit
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = @author.books.build(book_params)
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
+        format.html { redirect_to author_books_path(@book), notice: "Book was successfully created." }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -32,7 +33,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
+        format.html { redirect_to author_book_path(@author), notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -44,7 +45,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.html { redirect_to author_books_path(@author), notice: "Book was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -52,10 +53,14 @@ class BooksController < ApplicationController
   private
 
   def set_author
-    @book = Book.find(params[:id])
+    @book = @author.books.find(params[:id])
   end
 
   def book_params
     params.require(:book).permit(:name, :author_id)
+  end
+
+  def get_author
+    @author = Author.find(params[:author_id])
   end
 end
